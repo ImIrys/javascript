bde64fc27d370f60a0eb9c2adcf15208448eddaf
ImIrys <irys.lda@gmail.com>
2022-12-29 18:28:24 -0300

    Atualizando pastas

---
+++ 02 - variaveis/script.js
@@ -0,0 +1,106 @@
+// tipos primitivos
+
+// boolean
+/*var vOuF = false;
+console.log(typeof(vOuF));
+
+// number 
+var numeroQualquer= 1;
+console.log(typeof(numeroQualquer));
+
+// string ou caractere
+var nome = 'Irys';
+console.log(typeof(nome)); */
+
+// como declarar
+/* var variavel = 'Irys';
+variavel = 'Ellora';
+console.log(variavel); */
+
+/* let variavel2 = 'Vakinha';
+variavel2 = 'Megan';
+console.log(variavel2); */
+
+/* const constante = 'Irys';
+console.log(constante); */
+
+/*var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal () {
+    let escopoLocalInterno = 'local'
+    console.log(escopoLocalInterno);
+}
+
+escopoLocal(); */
+
+/*// atribuição
+var atribuicao = 'Irys';
+
+// comparação
+var comparacao = '0' == 0;
+console.log(comparacao);
+
+// comparação idêntica =mvalor e tipo idêntico
+var comparacaoIdentica = '0' === 0;
+console.log(comparacaoIdentica); */
+
+
+/*// Operadores aritméticos
+// adição
+var adicao = 1 + 1;
+console.log(adicao);
+
+// subtração
+var subtracao = 1 - 1;
+console.log(subtracao);
+
+// multiplicação
+var multiplicacao = 2 * 3;
+console.log(multiplicacao);
+
+// divisão real
+var divisaoReal = 6 / 2;
+console.log(divisaoReal);
+
+// divisão inteira ou o resto da divisão
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);
+
+// potenciação
+var potenciacao = 2 ** 10;
+console.log(potenciacao); */
+
+/*// Operadores relacionais - consultam a relação entre valores
+// maior que
+var maiorQue = 5 > 2;
+console.log(maiorQue);
+
+// menor que
+var menorQue = 5 < 2;
+console.log(menorQue);
+
+// maior ou igual a
+var maiorOuIgual = 5 >= 2;
+console.log(maiorOuIgual);
+
+// menor ou igual a
+var menorOuIgual = 5 <= 2;
+console.log(menorOuIgual); */
+
+// Operadores lógicos - consultam valores lógicos
+// && - "e" - considera que todos os valores sejam true
+var e = true && false;
+console.log(e);
+
+// || - "ou" - considera que qualquer valor seja true
+var ou = true || false;
+console.log(ou);
+
+// ! - "não" - inverte o valor de true para false ou vice-versa
+var nao = !true;
+console.log(nao);
+
+
+
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# javascript
-Pratice of functional programming using JS.
