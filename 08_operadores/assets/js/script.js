a5c13f2a9cdfe9444336639c44de8138c353af18
ImIrys <irys.lda@gmail.com>
2023-02-07 16:43:13 -0300

    Sintaxe e Operadores JS

---
+++ 08_operadores/assets/js/script.js
@@ -0,0 +1,6 @@
+// Atividade prática:
+// - Criar uma função que receba 2 números como parâmetros;
+// - Conferir se os números são iguais;
+// - Conferir se a soma dos números é maior que 10 ou menor que 20;
+// - Retornar uma string que diz: "Os números num1 e num2 não/são iguais. Sua Soma é soma, que é maior/menor que 10 e maior/menor que 20.
+
