38a6ef688f7e6aa6a38d94b84dd52678135d5ce1
ImIrys <irys.lda@gmail.com>
2023-02-16 18:04:33 -0300

    Atualização teste

---
+++ 02_variaveis/assets/js/tiposDados.js
@@ -0,0 +1,50 @@
+// Exemplo de String
+let nome = "Valentina";
+let sobrenome = "Sobral";
+// 1ª forma de concatenação
+/*
+nome.concat(sobrenome);
+let concatenado = nome.concat(sobrenome);
+
+console.log(concatenado); // vai juntar as variáveis
+*/
+
+// 2ª forma de concatenação
+/*
+concatenado = nome + " " + sobrenome; //vai dar espaço entre as variáveis
+
+console.log(concatenado); 
+*/
+
+// 3ª forma de concatenação
+/*
+concatenado = nome + "\n" + sobrenome;
+
+console.log(concatenado); //vai pular uma linha entre as variáveis
+*/
+
+// 4ª forma de concatenação
+/*
+concatenado = `${nome} ${sobrenome}`;
+
+console.log(concatenado);
+*/
+
+// 5ª forma de concatenação
+/*
+concatenado = `${nome} 
+${sobrenome}`;
+
+console.log(concatenado); // vai pular uma linha entre as variáveis
+*/
+
+let frase = "Hello, world. This is me. Life should be fun for everyone =)"
+frase.split("");
+
+
+
+
+
+
+
+
